7de82af5e8bf56bcbd30c3c819a77e6c54c8b1e4
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-11 16:54:38 -0700

    adding js file

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<title>Literary Mutilator</title>
 </head>
 <body>
-
 <p style="color:#FF0000">
 	Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.
 </p>
@@ -13,11 +12,9 @@
 	Shrimp tacos are tasty tacos! These tacos are lit 🔥. Add in a few el Pastor with guac and diced onions. BARBACOA!! Give me all the tacos, immediately. It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business. Give me all the tacos, immediately. I think I’ve overdosed on tacos. Tacos for breakfast, lunch and dinner. It’s taco Tuesday Monday. BARBACOA!! It’s taco time all the time. CARNITAS!! Can you put some peppers and onions on that?
 	</mark>
 </p>
-<br>
-<img src="kitty.jpg">
-</p>
+
 <p>
-	Sleep all day whilst slave is at work, play all night whilst slave is sleeping dont wait for the storm to pass, dance in the rain no, you can't close the door, i haven't decided whether or not i wanna go out or lick human with sandpaper tongue for mewl for food at 4am. Cats making all the muffins munch, munch, chomp, chomp and play time attack dog, run away and pretend to be victim eat the rubberband climb a tree, wait for a fireman jump to fireman then scratch his face. Meow. All of a sudden cat goes crazy give me some of your food give me some of your food give me some of your food meh, i don't want it. Fish i must find my red catnip fishy fish attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently human is washing you why halp oh the horror flee scratch hiss bite. Crash against wall but walk away like nothing happened meow but sniff catnip and act crazy so lick sellotape, so meow meow, i tell my human yet run in circles. Always ensure to lay down in such a manner that tail can lightly brush human's nose good now the other hand, too. Cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers lick the other cats. Floof tum, tickle bum, jellybean footies curly toes 𝕄𝔼𝕆𝕎 leave hair everywhere, or steal the warm chair right after you get up or munch, munch, chomp, chomp but drool.
+	Sleep <mark> all </mark> day whilst slave is at work, play all night whilst slave is sleeping dont wait for the storm to pass, dance in the rain no, you can't close the door, i haven't decided whether or not i wanna go out or lick human with sandpaper tongue for mewl for food at 4am. Cats making all the muffins munch, munch, chomp, chomp and play time attack dog, run away and pretend to be victim eat the rubberband climb a tree, wait for a fireman jump to fireman then scratch his face. Meow. All of a sudden cat goes crazy give me some of your food give me some of your food give me some of your food meh, i don't want it. Fish i must find my red catnip fishy fish attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently human is washing you why halp oh the horror flee scratch hiss bite. Crash against wall but walk away like nothing happened meow but sniff catnip and act crazy so lick sellotape, so meow meow, i tell my human yet run in circles. Always ensure to lay down in such a manner that tail can lightly brush human's nose good now the other hand, too. Cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers lick the other cats. Floof tum, tickle bum, jellybean footies curly toes 𝕄𝔼𝕆𝕎 leave hair everywhere, or steal the warm chair right after you get up or munch, munch, chomp, chomp but drool.
 </p>
 
 </body>
